f7339ecaead19d1cae6eb83118bc2b68dc8e24dd
Seth Holobaugh <seth.holobaugh@gmail.com>
2020-03-08 07:25:13 -0500

    Added tailwind

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-gray-800">
+  <nav class="bg-gray-900">
       <router-link to="/" class="font-semibold text-xl tracking-tight">Home</router-link>
       <router-link to="/Journal" class="font-semibold text-xl tracking-tight">Journal</router-link>
       <router-link to="/Login" class="font-semibold text-xl tracking-tight">Login</router-link>
