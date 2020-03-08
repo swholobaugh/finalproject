b03513b43218d8d3080e24bbe3b87786edb75679
Seth Holobaugh <seth.holobaugh@gmail.com>
2020-03-08 09:48:50 -0500

    Created JournalForm component

---
+++ src/components/JournalForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <label class="block">
+          <span class="text-gray-500">Comment</span>
+          <input v-model="comment" class="form-input mt-1 w-32" placeholder="Enter comment..." />
+      </label>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'JournalForm'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Journal.vue
+++ src/views/Journal.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <h1 class="p-2">Journal</h1>
+    <h1 class="p-4 text-center text-4xl">Journal Page</h1>
+    <div>
+      <JournalForm />
+    </div>
   </div>
 </template>
 
 <script>
+import JournalForm from '../components/JournalForm'
+
 export default {
     name: 'Journal',
     components: {
-
+      JournalForm
     }
 }
 </script>
