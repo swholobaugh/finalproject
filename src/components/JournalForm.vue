ecf467db31d1e284c1f3e52951866c443060f2ee
Seth Holobaugh <seth.holobaugh@gmail.com>
2020-03-08 19:43:36 -0500

    Updated Component

--- src/components/JournalForm.vue
+++ src/components/JournalForm.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>
-      <label class="block">
-          <div class="text-gray-500">Comment</div>
-          <input v-model="comment" class="form-input mt-1 w-32" placeholder="Enter comment..." />
+  <div class="flex">
+      <label class="flex-1">
+          <div class="text-black text-center">Date</div>
       </label>
-  </div>
+      <label class="flex-1">
+          <div class="text-black text-center">Symbol</div>
+      </label>
+      <label class="flex-1">
+          <div class="text-black text-center">Open Price</div>
+      </label>
+      <label class="flex-1">
+          <div class="text-black text-center">Closing Price</div>
+      </label>
+      <label class="flex-1">
+          <div class="text-black text-center">P/L</div>
+      </label>
+      <label class="flex-1">
+          <div class="text-black text-center">Balance</div>
+      </label>
+      <label class="flex-1">
+          <div class="text-black text-center">Comment</div>
+          <input v-model="comment" class="form-input mt-1 w-56" placeholder="Enter comment..." />
+      </label>
+    </div>
 </template>
 
 <script>
